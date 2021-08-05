7e13a7721feae6f92d4d81a50c1266e874a36581
Carlos Andrade <casdea@gmail.com>
2021-08-05 20:12:33 -0300

    Outra API

---
+++ CursoApiBackEnd/CursoApiBackuEnd/CursoApiBackuEnd/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using CursoApiBackuEnd.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoApiBackuEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    { 
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
