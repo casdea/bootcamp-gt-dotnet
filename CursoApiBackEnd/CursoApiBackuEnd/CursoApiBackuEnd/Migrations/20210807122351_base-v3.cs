bb81284fedec3c0d867d205fb533b5c8aa91b78c
Carlos Andrade <casdea@gmail.com>
2021-08-07 16:29:36 -0300

    Outra API

---
+++ CursoApiBackEnd/CursoApiBackuEnd/CursoApiBackuEnd/Migrations/20210807122351_base-v3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoApiBackuEnd.Migrations
+{
+    public partial class basev3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
